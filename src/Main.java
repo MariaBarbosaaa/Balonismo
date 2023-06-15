cb72ec4795372ce947362eae6f433038c6687382
MariaBarbosaaa <mb13052006@gmail.com>
2023-06-15 11:51:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        // Criar objetos ao passeio
+        Pessoas p1 = new Pessoas();
+        Pessoas p2;
+        p2 = new Pessoas();
+        //...
+        p1.nome = "Elliot Page";
+    }
+
+}
---
+++ src/Pessoas.java
@@ -0,0 +1,11 @@
+public class Pessoas {
+    //Atributos
+    String nome;
+
+
+
+    //Métodos
+    public void pesquisarEmpresa(){
+        
+    }
+}
